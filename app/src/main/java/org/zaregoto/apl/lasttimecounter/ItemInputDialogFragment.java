85bb13a19481e279bffc277bb504c60403e4b3ac
Ayumu WAKABAYASHI <waka@zaregoto.org>
2017-12-07 19:20:09 +0900

    add fab button and callback, call dummy dialog

---
+++ app/src/main/java/org/zaregoto/apl/lasttimecounter/ItemInputDialogFragment.java
@@ -0,0 +1,34 @@
+package org.zaregoto.apl.lasttimecounter;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class ItemInputDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View content = inflater.inflate(R.layout.fragment_item_input_dialog, null);
+
+        builder.setView(content);
+
+        builder.setMessage("設定")
+                .setNegativeButton("閉じる", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
--- app/src/main/java/org/zaregoto/apl/lasttimecounter/MainActivity.java
+++ app/src/main/java/org/zaregoto/apl/lasttimecounter/MainActivity.java
@@ -1,10 +1,18 @@
 package org.zaregoto.apl.lasttimecounter;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
+import java.util.*;
+
 public class MainActivity extends AppCompatActivity {
+
+    ItemInputDialogFragment itemInputDialog;
+    List<LastTimeItem> items = new ArrayList<>();
+    ItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +21,21 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lv = findViewById(R.id.mainlist);
         if (null != lv) {
-            
+            adapter = new ItemAdapter(this, 0, items);
+            lv.setAdapter(adapter);
         }
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        if (null != fab) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    itemInputDialog = new ItemInputDialogFragment();
+                    itemInputDialog.show(getFragmentManager(), "");
+                }
+            });
+        }
+
+
     }
 }
