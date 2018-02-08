2179ff5c26d47fc41305e0ffb539c4b8fe02806e
Ayumu WAKABAYASHI <waka@zaregoto.org>
2018-02-08 22:59:21 +0900

    temporary commit for autobackup daemon

---
+++ app/src/main/java/org/zaregoto/apl/lasttimecounter/service/CheckBackupAgent.java
@@ -0,0 +1,7 @@
+package org.zaregoto.apl.lasttimecounter.service;
+
+import android.app.backup.BackupAgent;
+import android.app.backup.BackupAgentHelper;
+
+public class CheckBackupAgent extends BackupAgentHelper {
+}
